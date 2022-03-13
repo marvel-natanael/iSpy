191a1cf5352d66410fef8445d45a587b5cb8863c
Vivian Azalia <55457999+vivianazalia@users.noreply.github.com>
2022-03-13 17:46:04 +0700

    Add shoot mechanic

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -1,16 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Player;
 
 public class NPC : MonoBehaviour
 {
+    [Header("Target")]
+    [SerializeField] protected GameObject targetPlayer;
+
     [Header("Properties")]
     [SerializeField]
     protected float delay;
-    protected float currentTime;
+    protected float timerToDelay;
+
+    protected FieldOfView fov;
+
+    [SerializeField] private float fireSpeed;
+    private float timerToFire;
+
+    public virtual void Start()
+    {
+        fov = GetComponent<FieldOfView>();
+        timerToDelay = delay;
+    }
 
     public virtual void Attack()
     {
+        timerToFire += Time.deltaTime;
+        if (timerToFire < fireSpeed) return;
+
+        if (fov.playersInRange.Count == 0) return;
+
+        var bulletPool = BulletPool.Instance.GetBullet();
+        if (bulletPool == null) return;
+
+        bulletPool.SetActive(true);
+        bulletPool.transform.position = transform.position;
+
+        var dir = targetPlayer.transform.position - transform.position;
+        var bullet = bulletPool.GetComponent<Bullet>();
+        bullet.Move(dir);
+
+        timerToFire = 0;
         Debug.Log(gameObject.name + " attack!");
     }
 
