8084565d26cdd1105e115df6e8769e364d4b3089
Vivian Azalia <55457999+vivianazalia@users.noreply.github.com>
2022-04-22 13:27:34 +0700

    Clear room

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using Player;
+using UnityEngine.SceneManagement;
 
 public class GameManager : NetworkBehaviour
 {
@@ -17,7 +18,10 @@ public class GameManager : NetworkBehaviour
     {
         if (newVal)
         {
-            Debug.Log("GAME OVER");
+            if (isClient)
+            {
+                //StartCoroutine(ClearRoom());
+            }
         }
     }
 
@@ -34,12 +38,68 @@ public class GameManager : NetworkBehaviour
         
     }
 
+    private void Update()
+    {
+        StartCoroutine(ClearRoom());
+    }
+
 
     public void GameOver()
     {
         if(playersCount == 1)
         {
             gameOver = true;
+
+            //if (isServer)
+            //{
+            //    StartCoroutine(ServerReturnToLobbyCoroutine(2f));
+            //}
         }
+    }
+
+    private IEnumerator ClearRoom()
+    {
+        var manager = NetworkManager.singleton as LobbyNetworkManager;
+        
+        yield return new WaitForSeconds(2f);
+        
+        if (gameOver)
+        {
+            if (isClient)
+            {
+                Debug.Log("STOP CLIENT");
+                manager.StopClient();
+            }
+
+            else if (isServer)
+            {
+                Debug.Log("Conn Count : " + NetworkServer.connections.Count);
+                if(NetworkServer.connections.Count <= 0)
+                {
+                    manager.ResetGame();
+                    //var lobbyScene = NetworkManager.singleton.onlineScene;
+                    //SceneManager.LoadScene(lobbyScene);
+                }
+            }
+        }
+    }
+
+    private IEnumerator ServerReturnToLobbyCoroutine(float delay)
+    {
+        var manager = NetworkManager.singleton as LobbyNetworkManager;
+        yield return new WaitForSeconds(delay);
+        manager.ResetGame();
+    }
+
+    private void ServerReturnToLobby()
+    {
+        var manager = NetworkManager.singleton as LobbyNetworkManager;
+        manager.ResetGame();
+    }
+
+    public override void OnStopClient()
+    {
+        base.OnStopClient();
+        Debug.Log("ON STOP CLIENT CALLED ");
     }
 }
